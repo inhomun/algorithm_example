3df9768efd0cea2b6b569524198740e3f3c04077
inhomun <inho8063@icloud.com>
2023-10-20 17:06:27 +0900

    programmers_n의배수

---
+++ forStudy/forStudy/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  forStudy
+//
+//  Created by 문인호 on 2023/01/27.
+//
+
+import Foundation
+
+func solution(_ num:Int, _ n:Int) -> Int {
+    let a = (num%n == 0) ? 1 : 0
+    return a
+}
