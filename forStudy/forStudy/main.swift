600d97646cdc3d07372aba9fc08fcadc5f52d1ac
inhomun <inho8063@icloud.com>
2023-10-22 13:40:04 +0900

    programmers_홀짝에 따라 다른 값 반환하기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,10 +4,23 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-
 import Foundation
 
-func solution(_ num:Int, _ n:Int) -> Int {
-    let a = (num%n == 0) ? 1 : 0
-    return a
+func solution(_ n:Int) -> Int {
+    var sum = 0
+    if n%2 == 1 {
+        for i in 0...n {
+            if i%2 == 1 {
+                sum += i
+            }
+        }
+    }
+    else {
+        for i in 0...n+1 {
+            if i%2 == 0 {
+                sum += i*i
+            }
+        }
+    }
+    return sum
 }
