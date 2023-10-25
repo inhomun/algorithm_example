06237ca448105a0cd55520686942b02e384fe3b0
inhomun <inho8063@icloud.com>
2023-10-25 17:46:45 +0900

    programmers_코드_처리하기

--- coding_test/runningContest.py
+++ coding_test/runningContest.py
@@ -1,10 +1,18 @@
-def solution(players, callings):
-    hashmap = dict()
-    for i,v in enumerate(players):
-        hashmap[v] = i
-    for call in callings:
-        pre,post = hashmap[call] - 1,hashmap[call]
-        hashmap[players[pre]] = post
-        hashmap[players[post]] = pre
-        players[pre],players[post] = players[post],players[pre]
-    return players
+def solution(code):
+    mode = 0
+    arr = []
+    for idx, i in enumerate(code):
+        if i == '1':
+            mode = 0 if mode == 1 else 1
+        elif mode == 0:
+            if idx % 2 == 0:
+                arr.append(i)
+        elif mode == 1:
+            if idx % 2 == 1:
+                arr.append(i)
+
+    answer = ''.join(arr)
+    if answer == '':
+        return 'EMPTY'
+    else:
+        return answer
