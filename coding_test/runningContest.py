2636364fe80df1489962e2d70f72c28cec89bcff
inhomun <inho8063@icloud.com>
2023-10-25 20:29:14 +0900

    주사위_게임2

--- coding_test/runningContest.py
+++ coding_test/runningContest.py
@@ -1,7 +1,7 @@
-def solution(a, d, included):
-    answer = 0
-    for idx, i in enumerate(included):
-        if i:
-            answer += (a + idx * d)
-
-    return answer
+def solution(a, b, c):
+    if a==b and b==c:
+        return (a+b+c)*(pow(a, 2)+pow(b, 2)+pow(c, 2))*(pow(a, 3)+pow(b, 3)+pow(c, 3))
+    elif a==b or b==c or a==c:
+        return (a+b+c)*(pow(a, 2)+pow(b, 2)+pow(c, 2))
+    elif a!=b and b!=c and a!=c:
+        return a+b+c
