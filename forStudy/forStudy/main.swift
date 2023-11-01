3adea358b5b943a24982c792f85951569fefcf42
inhomun <inho8063@icloud.com>
2023-11-01 15:04:44 +0900

    programmers_수열과_구간_쿼리_2

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,14 +4,22 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-import Foundation
-
-func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
-    var answer = arr
-    for i in queries {
-        var tmp = answer[i[0]]
-        answer[i[0]] = answer[i[1]]
-        answer[i[1]] = tmp
+func solution(_ arr: [Int], _ queries: [[Int]]) -> [Int] {
+    var result = [Int]()
+    
+    for query in queries {
+        let s = query[0]
+        let e = query[1]
+        let k = query[2]
+        
+        let subArr = Array(arr[s...e])
+        let filteredArr = subArr.filter { $0 > k }
+        if let minValue = filteredArr.min() {
+            result.append(minValue)
+        } else {
+            result.append(-1)
+        }
     }
-    return answer
+    
+    return result
 }
