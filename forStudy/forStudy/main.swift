ee28bb02b4e5c190639d04747409ac99ed0faa19
inhomun <inho8063@icloud.com>
2023-12-18 18:30:16 +0900

    programmers_문자열_나누기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,28 +4,33 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ keymap: [String], _ targets: [String]) -> [Int] {
-    var keys: [Character: (Int, Int)] = [:]
-    for (i, key) in keymap.enumerated() {
-        for (j, char) in key.enumerated() {
-            if let existingValue = keys[char], existingValue.1 <= j+1 {
-                continue
-            }
-            keys[char] = (i+1, j+1)
-        }
-    }
+import Foundation
 
-    var result: [Int] = []
-    for target in targets {
-        var count = 0
-        for char in target {
-            guard let key = keys[char] else {
-                count = -1
-                break
-            }
-            count += key.1
+func solution(_ s:String) -> Int {
+    var firstChar : Character?
+    var result = 0
+    var cnt = 0
+    var notcnt = 0
+    for i in s {
+        if firstChar == nil {
+            firstChar = i
         }
-        result.append(count)
+        
+        if i == firstChar {
+            cnt += 1
+        } else {
+            notcnt += 1
+        }
+        
+        if cnt == notcnt {
+            result += 1
+            cnt = 0
+            notcnt = 0
+            firstChar = nil
+        }
     }
+        if cnt != 0 || notcnt != 0 {
+            result += 1
+        }
     return result
 }
