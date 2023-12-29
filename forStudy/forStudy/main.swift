8464fada2d4ca78de5909b3cefaecade7f6d2091
inhomun <inho8063@icloud.com>
2023-12-29 22:16:41 +0900

    programmers_옹알이

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,14 +4,22 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
+
 import Foundation
 
-func solution(_ cipher:String, _ code:Int) -> String {
-    var answer = ""
-    for (idx, i) in cipher.enumerated() {
-        if (idx+1) % code == 0 {
-            answer.append(String(i))
+func solution(_ babbling:[String]) -> Int {
+    let filter = ["aya", "ye", "woo", "ma"]
+    var count = 0
+    for i in babbling {
+        var tmp = ""
+        for j in filter {
+            if i.range(of: j) != nil {
+            tmp += j
+        }
+        }
+        if tmp.count == i.count {
+            count += 1
         }
     }
-    return answer
+    return count
 }
