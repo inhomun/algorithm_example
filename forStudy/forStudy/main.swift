5d0c28520501b0abfd48672c2c271e6dcf084051
inhomun <inho8063@icloud.com>
2024-01-03 15:27:03 +0900

    programmers_없는_숫자_더하기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,34 +6,14 @@
 //
 import Foundation
 
-func solution(_ number:[Int]) -> Int {
-    var answer = 0
-    func combinations<T>(_ array: [T], _ k: Int) -> [[T]] {
-    if k == 0 {
-        return [[]]
-    }
-
-    guard !array.isEmpty else {
-        return []
-    }
-
-    let head = array.first!
-    let subarray = Array(array.dropFirst())
-
-    let headCombinations = combinations(subarray, k - 1).map { [head] + $0 }
-    let tailCombinations = combinations(subarray, k)
-
-    return headCombinations + tailCombinations
-}
-    let result = combinations(number, 3)
-    for i in result {
-        var sum = 0
-        for j in i {
-            sum += j
-        }
-        if sum == 0 {
-            answer += 1
+func solution(_ numbers:[Int]) -> Int {
+    var filter = [0,1,2,3,4,5,6,7,8,9]
+    for i in numbers {
+        for (idx,j) in filter.enumerated() {
+            if i == j {
+                filter.remove(at: idx)
+            }
         }
     }
-    return answer
+    return filter.reduce(0, +)
 }
