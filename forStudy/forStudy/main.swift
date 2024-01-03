3573fb93a107f216c045b3c59079ef9215eba2e2
inhomun <inho8063@icloud.com>
2024-01-03 15:03:12 +0900

    programmers_삼총사

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,27 +4,35 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-
 import Foundation
 
-func solution(_ k:Int, _ score:[Int]) -> [Int] {
-    var tmp = [Int]()
-    var answer = [Int]()
-    for (idx, i) in score.enumerated() {
-        if idx < k {
-            tmp.append(i)
-            tmp.sort(by: { $0 > $1 } )
-            answer.append(tmp[idx])
+func solution(_ number:[Int]) -> Int {
+    var answer = 0
+    func combinations<T>(_ array: [T], _ k: Int) -> [[T]] {
+    if k == 0 {
+        return [[]]
+    }
+
+    guard !array.isEmpty else {
+        return []
+    }
+
+    let head = array.first!
+    let subarray = Array(array.dropFirst())
+
+    let headCombinations = combinations(subarray, k - 1).map { [head] + $0 }
+    let tailCombinations = combinations(subarray, k)
+
+    return headCombinations + tailCombinations
+}
+    let result = combinations(number, 3)
+    for i in result {
+        var sum = 0
+        for j in i {
+            sum += j
         }
-        else {
-            if i > tmp[k-1] {
-                tmp[k-1] = i
-                tmp.sort(by: { $0 > $1 } )
-                answer.append(tmp[k-1])
-            }
-            else {
-                answer.append(tmp[k-1])
-            }
+        if sum == 0 {
+            answer += 1
         }
     }
     return answer
