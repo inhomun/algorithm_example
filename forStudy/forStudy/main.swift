a8fa87b1401a696d833d27dd415fbd1b5ea04646
inhomun <inho8063@icloud.com>
2024-01-08 14:06:50 +0900

    programmers_부족한_금액_계산

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,17 +4,24 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
+
 import Foundation
 
-func solution(_ strArr:[String]) -> [String] {
-    var answer = [String]()
-    for i in strArr {
-        if i.contains("ad") {
-            continue
+func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
+    var answer:Int64 = -1
+    func fact (_ num : Int ) -> Int {
+        if num == 0 {
+            return 0
         }
         else {
-            answer.append(i)
+        return num * price + fact(num-1)
         }
     }
-    return answer
+    let num = fact(count)
+    if num > money {
+        return Int64(num - money)
+    }
+    else {
+        return 0
+    }
 }
