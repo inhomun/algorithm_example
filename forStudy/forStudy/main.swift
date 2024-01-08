84179e0e220d0fbca87dd5a306dde90cef0bc963
inhomun <inho8063@icloud.com>
2024-01-08 22:05:27 +0900

    programmers_3진법_뒤집기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,21 +6,9 @@
 //
 import Foundation
 
-func solution(_ left:Int, _ right:Int) -> Int {
-    var answer = 0
-    for i in left...right {
-        var tmp = 0
-        for j in 1...i {
-            if i % j == 0 {
-                tmp += 1
-            }
-        }
-        if tmp % 2 == 0 {
-                answer += i
-            }
-            else {
-                answer -= i
-            }
-    }
-    return answer
+func solution(_ n:Int) -> Int {
+    let ternary = String(n, radix: 3)
+    let reversedTernary = String(ternary.reversed())
+    let decimal = Int(reversedTernary, radix: 3)
+    return decimal!
 }
