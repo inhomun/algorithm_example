85b18646eed04724fe6cc3821bcc4b1d5803bc08
inhomun <inho8063@icloud.com>
2024-01-10 20:38:50 +0900

    programmers_두_개_뽑아서_더하기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,12 +4,12 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-import Foundation
-
-func solution(_ a:[Int], _ b:[Int]) -> Int {
-    var answer = 0
-    for i in 0...a.count-1 {
-        answer += a[i]*b[i]
+var result = [Int]()
+    for i in 0..<numbers.count {
+        for j in i+1..<numbers.count {
+            result.append(numbers[i] + numbers[j])
+        }
     }
-    return answer
+    let sortedResult = Array(Set(result)).sorted()
+    return sortedResult
 }
