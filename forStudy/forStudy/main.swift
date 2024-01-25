1781b8ab025b48ff255d1c54b9a45c6e0e53af5d
inhomun <inho8063@icloud.com>
2024-01-25 12:17:54 +0900

    programmers_더큰숫자

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,12 +4,8 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ seoul:[String]) -> String {
-    var answer = 0
-    for (idx, i) in seoul.enumerated() {
-        if i == "Kim" {
-            answer = idx
-        }
-    }
-    return "김서방은 \(answer)에 있다"
+import Foundation
+
+func solution(_ a:Int, _ b:Int) -> Int {
+    return Int(String(a)+String(b))! > Int(String(b)+String(a))! ? Int(String(a)+String(b))! : Int(String(b)+String(a))!
 }
