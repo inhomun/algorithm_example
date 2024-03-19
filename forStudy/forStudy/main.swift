99c3689b93e36d539030fde507cdbc8773e01dfb
inhomun <inho8063@icloud.com>
2024-03-19 16:00:49 +0900

    programmers_자연수_뒤집기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,19 +4,12 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-import Foundation
-
-func solution(_ n:Int) -> Int
-{
+func solution(_ n:Int64) -> [Int] {
     var tmp = n
-    var answer:Int = 0
+    var answer = [Int]()
     repeat {
-        answer += (tmp % 10)
-        tmp = tmp / 10
+        answer.append(Int(tmp % 10))
+        tmp /= 10
     } while tmp > 0
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print("Hello Swift")
-    
     return answer
 }
