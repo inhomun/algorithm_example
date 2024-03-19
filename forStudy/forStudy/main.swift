5b52bc4323fc0b1af13d62abfe57042f5ca0140d
inhomun <inho8063@icloud.com>
2024-03-19 16:35:12 +0900

    programmers_정수_제곱근_판별

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,13 +4,13 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
+import Foundation
 func solution(_ n:Int64) -> Int64 {
-    var tmp = n
-    var answer = [Int]()
-    repeat {
-        answer.append(Int(tmp % 10))
-        tmp /= 10
-    } while tmp > 0
-    answer = answer.sorted(by: > )
-    return Int64(answer.reduce(0, { $0 * 10 + $1 }))
+    let squearRoot = sqrt(Double(n))
+    if squearRoot == Double(Int64(squearRoot)) {
+        return Int64(pow(squearRoot+1, 2))
+    }
+    else {
+        return -1
+    }
 }
