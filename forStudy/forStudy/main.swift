f358001627d7398c95b18dab97d9d3567308be81
inhomun <inho8063@icloud.com>
2024-03-19 16:06:44 +0900

    programmers_정수_내림차순_배치

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,12 +4,13 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ n:Int64) -> [Int] {
+func solution(_ n:Int64) -> Int64 {
     var tmp = n
     var answer = [Int]()
     repeat {
         answer.append(Int(tmp % 10))
         tmp /= 10
     } while tmp > 0
-    return answer
+    answer = answer.sorted(by: > )
+    return Int64(answer.reduce(0, { $0 * 10 + $1 }))
 }
