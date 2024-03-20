2ed8fcec60bac5b3d9a42a20f345f49def519e25
inhomun <inho8063@icloud.com>
2024-03-20 17:02:42 +0900

    programmers_행렬의_덧셈

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,15 +4,14 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ phone_number:String) -> String {
-    var answer = ""
-    for (idx, i) in phone_number.enumerated() {
-        if idx < phone_number.count - 4 {
-            answer += "*"
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var result: [[Int]] = []
+    for i in 0..<arr1.count {
+        var row: [Int] = []
+        for j in 0..<arr1[0].count {
+            row.append(arr1[i][j] + arr2[i][j])
         }
-        else {
-            answer += "\(i)"
-        }
+        result.append(row)
     }
-    return answer
+    return result
 }
