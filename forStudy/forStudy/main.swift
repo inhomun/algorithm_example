e25e399c46cdc01a5e550dca0b2e34f742e31d32
inhomun <inho8063@icloud.com>
2024-03-22 20:01:35 +0900

    programmers_직사각형_별찍기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,10 +4,15 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ x:Int, _ n:Int) -> [Int64] {
-    var answer = [Int64]()
-    for i in 1...n {
-        answer.append(Int64(i*x))
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for i in 1...b {
+    var tmp = ""
+    for j in 1...a {
+        tmp += "*"
     }
-    return answer
+    print("\(tmp)")
 }
