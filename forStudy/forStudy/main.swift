24d4b6c712d8d762c42930b9435113d0ce677b0c
inhomun <inho8063@icloud.com>
2024-03-22 19:45:59 +0900

    x만큼_간격이_있는_n개_숫자

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,14 +4,10 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    var result: [[Int]] = []
-    for i in 0..<arr1.count {
-        var row: [Int] = []
-        for j in 0..<arr1[0].count {
-            row.append(arr1[i][j] + arr2[i][j])
-        }
-        result.append(row)
+func solution(_ x:Int, _ n:Int) -> [Int64] {
+    var answer = [Int64]()
+    for i in 1...n {
+        answer.append(Int64(i*x))
     }
-    return result
+    return answer
 }
