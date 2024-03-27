f10a482e60d743101364962cf43fa566f2a6a059
inhomun <inho8063@icloud.com>
2024-03-27 17:32:36 +0900

    programmers_예산

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,40 +6,18 @@
 //
 import Foundation
 
-import Foundation
-
-func solution(_ nums:[Int]) -> Int {
-    func isPrime(_ number: Int) -> Bool {
-    if number <= 1 {
-        return false
-    }
-    if number <= 3 {
-        return true
-    }
-    if number % 2 == 0 || number % 3 == 0 {
-        return false
-    }
-    var i = 5
-    while i * i <= number {
-        if number % i == 0 || number % (i + 2) == 0 {
-            return false
-        }
-        i += 6
-    }
-    return true
-}
+func solution(_ d:[Int], _ budget:Int) -> Int {
+    let a = d.sorted(by: <)
+    var b = 0
     var answer = 0
-    for i in 0..<nums.count {
-        for j in i+1..<nums.count {
-            for k in j+1..<nums.count {
-                if isPrime(nums[i]+nums[j]+nums[k]) {
-                    answer += 1
-                }
-            }
+    for i in a {
+        if i + b > budget {
+            return answer
+        }
+        else {
+            b += i
+            answer += 1
         }
     }
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print("Hello Swift")
     return answer
 }
-
