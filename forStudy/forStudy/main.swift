3ce5f6fd8879139aedec272face925f77f9cf912
inhomun <inho8063@icloud.com>
2024-03-27 14:47:20 +0900

    programmers_소수_만들기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,31 +6,40 @@
 //
 import Foundation
 
-func solution(_ s:String, _ n:Int) -> String {
-    var answer = ""
-    for i in s {
-        if i == " " {
-            answer += " "
+import Foundation
+
+func solution(_ nums:[Int]) -> Int {
+    func isPrime(_ number: Int) -> Bool {
+    if number <= 1 {
+        return false
+    }
+    if number <= 3 {
+        return true
+    }
+    if number % 2 == 0 || number % 3 == 0 {
+        return false
+    }
+    var i = 5
+    while i * i <= number {
+        if number % i == 0 || number % (i + 2) == 0 {
+            return false
         }
-        else {
-            var a = 0
-            var b = i.asciiValue!
-            repeat {
-                if b + 1 > 122 {
-                    b = 97
-                    a += 1
+        i += 6
+    }
+    return true
+}
+    var answer = 0
+    for i in 0..<nums.count {
+        for j in i+1..<nums.count {
+            for k in j+1..<nums.count {
+                if isPrime(nums[i]+nums[j]+nums[k]) {
+                    answer += 1
                 }
-                else if b + 1 > 90 && b < 97 {
-                    b = 65
-                    a += 1
-                }
-                else {
-                b += 1
-                a += 1
-                }
-            } while n != a
-            answer += String(Character(UnicodeScalar(b)))
+            }
         }
     }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
     return answer
 }
+
