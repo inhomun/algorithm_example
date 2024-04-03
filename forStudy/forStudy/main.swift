14dac0b687f0d2409c89dcdc763a42d6d5bc033d
inhomun <inho8063@icloud.com>
2024-04-03 16:52:54 +0900

    programmer_올바른_괄호

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,16 +6,25 @@
 //
 import Foundation
 
-func solution(_ A:[Int], _ B:[Int]) -> Int
+func solution(_ s:String) -> Bool
 {
-    var ans = 0
-    let a = A.sorted(by: <)
-    let b = B.sorted(by: >)
-    for i in 0..<A.count {
-        ans += a[i]*b[i]
+    var ans:Bool = false
+    var tmp = [Int]()
+    for i in s {
+        if i == "(" {
+            tmp.append(1)
+        }
+        else {
+            if tmp.contains(1) {
+                var a = tmp.popLast()
+            }
+            else {
+                return false
+            }
+        }
     }
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     print("Hello Swift")
 
-    return ans
+    return tmp.isEmpty
 }
