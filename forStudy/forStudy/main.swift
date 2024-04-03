4b33f848e47678e6978d774d9362c2c875976f03
inhomun <inho8063@icloud.com>
2024-04-03 16:07:05 +0900

    programmers_가장_많이_받은_선물

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,25 +6,45 @@
 //
 import Foundation
 
-func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    let day = today.components(separatedBy: ".").compactMap{ Int($0)! }
-    let target = day[0]*12*28+day[1]*28+day[2]
-    var term = [[String]]()
-    for k in terms {
-        term.append(k.components(separatedBy: " "))
-    }
-    var answer = [Int]()
-    for (idx, i) in privacies.enumerated() {
+func solution(_ friends:[String], _ gifts:[String]) -> Int {
+    // a - 준 사람 b - 받은 사람 gifts
+    var giftScore : [Int] = Array(repeating: 0, count: friends.count)
+    var giftRelation : [[Int]] = Array(repeating: Array(repeating: 0,count: friends.count), count: friends.count )
+    var answer : [Int] = Array(repeating: 0, count: friends.count)
+    for i in gifts {
         let a = i.components(separatedBy: " ")
-        var thisDay = a[0].components(separatedBy: ".").compactMap{ Int($0)! }
-        for j in term {
-            if a[1] == j[0] {
-                thisDay[1] += Int(j[1])!
-                if target >= thisDay[0]*12*28+thisDay[1]*28+thisDay[2] {
-                    answer.append(idx+1)
+        var b = friends.firstIndex(of: a[0])!
+        var c = friends.firstIndex(of: a[1])!
+        giftRelation[b][c] += 1
+        giftScore[b] += 1
+        giftScore[c] -= 1
+    }
+    for j in 0..<friends.count {
+        for k in j+1..<friends.count {
+            if giftRelation[j][k] > giftRelation[k][j] {
+                answer[j] += 1
+            }
+            else if giftRelation[k][j] > giftRelation[j][k] {
+                answer[k] += 1
+            }
+            else if giftRelation[j][k] == giftRelation[k][j] && giftRelation[j][k] == 0 {
+                if giftScore[j] > giftScore[k] {
+                    answer[j] += 1
+                }
+                else if giftScore[k] > giftScore[j] {
+                    answer[k] += 1
+                }
+            }
+            else if giftRelation[j][k] == giftRelation[k][j] {
+                if giftScore[j] > giftScore[k] {
+                    answer[j] += 1
+                }
+                else if giftScore[k] > giftScore[j] {
+                    answer[k] += 1
                 }
             }
         }
     }
-    return answer
+
+    return answer.max()!
 }
