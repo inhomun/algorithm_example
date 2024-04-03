0eb93233c1b115ec75f314079cd4424b64515a31
inhomun <inho8063@icloud.com>
2024-04-03 13:19:40 +0900

    programmers_개인정보_수집_유효기간

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,25 +6,23 @@
 //
 import Foundation
 
-func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
-    var b = board
-    var answer = 0
-    var stack = [Int]()
-    for i in moves {
-        for j in 0..<b.count {
-            if b[j][i-1] == 0 {
-                continue
-            }
-            else {
-                if b[j][i-1] == stack.last {
-                    stack.removeLast()
-                    answer += 2
+func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
+    let day = today.components(separatedBy: ".").compactMap{ Int($0)! }
+    let target = day[0]*12*28+day[1]*28+day[2]
+    var term = [[String]]()
+    for k in terms {
+        term.append(k.components(separatedBy: " "))
+    }
+    var answer = [Int]()
+    for (idx, i) in privacies.enumerated() {
+        let a = i.components(separatedBy: " ")
+        var thisDay = a[0].components(separatedBy: ".").compactMap{ Int($0)! }
+        for j in term {
+            if a[1] == j[0] {
+                thisDay[1] += Int(j[1])!
+                if target >= thisDay[0]*12*28+thisDay[1]*28+thisDay[2] {
+                    answer.append(idx+1)
                 }
-                else {
-                    stack.append(b[j][i-1])
-                }
-                b[j][i-1] = 0
-                break
             }
         }
     }
