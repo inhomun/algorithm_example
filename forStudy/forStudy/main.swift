4dd6024b3bf7213072303854a899440331c212a4
inhomun <inho8063@icloud.com>
2024-04-11 11:48:23 +0900

    programmers_카펫

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,22 +6,30 @@
 //
 import Foundation
 
-func solution(_ n:Int) -> Int {
-    var arr = [Int]()
-    for i in 0..<n {
-        if i == 0 {
-            arr.append(1)
-        }
-        else if i == 1 {
-            arr.append(1)
-        }
-        else if i == 2 {
-            arr.append(2)
-        }
-        else {
-            arr.append((arr[i-2] + arr[i-1]) % 1234567)
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    var Arr = [[Int]]()
+    let total = brown + yellow
+    for i in 1...total/2 {
+        if total % i == 0 && i >= 3{
+            Arr.append([i, total/i])
         }
     }
-    var answer = arr.popLast()!
-    return answer
+    for j in Arr {
+        var b = 0
+        var y = 0
+        for k in 0..<j[0] {
+            for l in 0..<j[1] {
+                if k == 0 || k == j[0]-1 || l == 0 || l == j[1]-1 {
+                    b += 1
+                }
+                else {
+                    y += 1
+                }
+            }
+        }
+        if b == brown && y == yellow {
+            return [max(j[0],j[1]), min(j[0],j[1])]
+        }
+    }
+    return [1, 1]
 }
