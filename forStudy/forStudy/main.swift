205d7a5f42f94203e3027f0184bb7dd28b1fa5a4
inhomun <inho8063@icloud.com>
2024-04-11 20:18:36 +0900

    programmers_H_index

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,29 +6,13 @@
 //
 import Foundation
 
-func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    var queue = discount
-    var dic = Dictionary(uniqueKeysWithValues: zip(want, number))
-    var cnt = 0
+func solution(_ citations:[Int]) -> Int {
+    let arr = citations.sorted(by: >)
     var answer = 0
-    while !queue.isEmpty {
-        let a = queue.removeFirst()
-        cnt += 1
-        if dic.contains { $0.key == a } {
-            dic[a]! -= 1
-        }
-        var tmp = 0
-        for i in dic {
-            if i.value == 0 {
-                tmp += 1
-            }
-            if tmp == want.count {
-                answer += 1
-            }
-        }
-        if cnt >= 10 {
-            if dic.contains { $0.key == discount[cnt - 10] } {
-            dic[discount[cnt - 10]]! += 1
+    for j in 0...arr.count {
+        for (idx,i) in arr.enumerated() {
+            if idx + 1 >= j && i >= j {
+                answer = j
             }
         }
     }
