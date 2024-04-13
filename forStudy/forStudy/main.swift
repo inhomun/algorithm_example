9cb78917db04b7a2847d117a156c2bad380849f4
inhomun <inho8063@icloud.com>
2024-04-13 11:20:40 +0900

    programmers_멀리_뛰기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,17 +4,19 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-import Foundation
-
-func solution(_ citations:[Int]) -> Int {
-    let arr = citations.sorted(by: >)
-    var answer = 0
-    for j in 0...arr.count {
-        for (idx,i) in arr.enumerated() {
-            if idx + 1 >= j && i >= j {
-                answer = j
-            }
+func solution(_ n:Int) -> Int {
+    var arr = [Int]()
+    arr.append(1)
+    for i in 1...n {
+        if i == 1 {
+            arr.append(1)
+        }
+        else if i == 2 {
+            arr.append(2)
+        }
+        else {
+            arr.append((arr[i-2] + arr[i-1])%1234567)
         }
     }
-    return answer
+    return arr.last!
 }
