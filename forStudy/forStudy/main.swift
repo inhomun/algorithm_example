5ac3db4d1bcf48ed000e15dbf5c2feb97cfdec91
inhomun <inho8063@icloud.com>
2024-04-13 12:33:03 +0900

    programmers_예상_대진표

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,19 +4,21 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ n:Int) -> Int {
-    var arr = [Int]()
-    arr.append(1)
-    for i in 1...n {
-        if i == 1 {
-            arr.append(1)
-        }
-        else if i == 2 {
-            arr.append(2)
-        }
-        else {
-            arr.append((arr[i-2] + arr[i-1])%1234567)
-        }
+import Foundation
+
+func solution(_ n:Int, _ a:Int, _ b:Int) -> Int
+{
+    var valA = a
+    var valB = b
+    var answer = 0
+    while !(valA == valB) {
+        valA = Int(ceil(Double(valA) / 2))
+        valB = Int(ceil(Double(valB) / 2))
+        answer += 1
     }
-    return arr.last!
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+
+    return answer
 }
