dd9e1e834a0acbe1f52e5aca3a636dcdff7d4556
inhomun <inho8063@icloud.com>
2024-04-14 12:30:42 +0900

    programmers_캐시

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,21 +4,26 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-import Foundation
-
-func solution(_ n:Int, _ a:Int, _ b:Int) -> Int
-{
-    var valA = a
-    var valB = b
+func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
+    var tmp = [String]()
     var answer = 0
-    while !(valA == valB) {
-        valA = Int(ceil(Double(valA) / 2))
-        valB = Int(ceil(Double(valB) / 2))
-        answer += 1
+    if cacheSize == 0 {
+        return cities.count * 5
     }
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print("Hello Swift")
-
+    for i in cities {
+        var a = i.lowercased()
+        if !tmp.contains(a) {
+            if tmp.count == cacheSize {
+                tmp.removeFirst()
+            }
+            tmp.append(a)
+            answer += 5
+        }
+        else if tmp.contains(a) {
+            tmp.remove(at: tmp.firstIndex(of: a)!)
+            tmp.append(a)
+            answer += 1
+        }
+    }
     return answer
 }
