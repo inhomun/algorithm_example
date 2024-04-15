231ec4f302401f4caf74a057e98ab5328a4517c0
inhomun <inho8063@icloud.com>
2024-04-15 19:28:56 +0900

    programmers_뒤에_있는_큰_수

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,22 +4,17 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
-    var num = 0
-    var queue = [Character]()
-    var answer = ""
-    while queue.count < t * m {
-        queue += Array(String(num, radix: n))
-        num += 1
-    }
-    var a = queue.enumerated().filter{index, _ in
-                                     return m == p ? (index + 1) % m == 0 : (index + 1) % m == p}.map{ _, element in
-                                                                     return element}
-    for j in a {
-        answer += String(j).uppercased
-        if answer.count == t {
-            return answer
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var number = numbers
+    var stack = [Int]()
+    var answer = Array(repeating: -1, count: numbers.count)
+    for i in 0..<number.count {
+        while !stack.isEmpty && number[stack.last!] < number[i] {
+            answer[stack.popLast()!] = number[i]
         }
+        stack.append(i)
     }
-    return String(a).uppercased()
+    return answer
 }
