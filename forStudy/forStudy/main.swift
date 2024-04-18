576f208414f71e00ec0a963b1610630254a7c5dd
inhomun <inho8063@icloud.com>
2024-04-18 17:56:30 +0900

    programmers_주차_요금_계산

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,15 +6,48 @@
 //
 import Foundation
 
-func solution(_ land:[[Int]]) -> Int{
-    var land = land
-    for i in 0..<(land.count-1) {
-        land[i+1][0] += max(land[i][1], land[i][2], land[i][3])
-        land[i+1][1] += max(land[i][0], land[i][2], land[i][3])
-        land[i+1][2] += max(land[i][0], land[i][1], land[i][3])
-        land[i+1][3] += max(land[i][0], land[i][1], land[i][2])
+func solution(_ fees:[Int], _ records:[String]) -> [Int] {
+    var str = records.map{ $0.split(separator: " ").map(String.init) }
+    str.sort { $0[1] == $1[1] ? $0[0] < $1[0] : $0[1] < $1[1] }
+    var num = [String]()
+    var times = [String:Int]()
+    var answer = [String:Int]()
+    var b = [Int]()
+    while !str.isEmpty {
+        var a = str.removeFirst()
+        if a[2] == "IN" {
+            num.append(a[1])
+            let time = a[0].split(separator: ":").map { Int($0)! }
+            times[a[1], default: 0] = time[0] * 60 + time[1]
+        }
+        else {
+            var b = num.popLast()!
+            let time = a[0].split(separator: ":").map { Int($0)! }
+            answer[a[1], default: 0] += (time[0] * 60 + time[1] - times[b]!)
+        }
     }
- 
-    var answer = land.popLast()!
-    return answer.max()!
+    for i in num {
+        answer[i, default: 0] += (23 * 60 + 59) - times[i]!
+    }
+    for j in answer {
+        if fees[0] >= j.value {
+            answer[j.key] = fees[1]
+        }
+        else {
+            let double = Double(j.value - fees[0]) / Double(fees[2])
+            let int = (j.value - fees[0]) / fees[2]
+            var time = 0
+            
+            if double > Double(int){
+                time = int + 1
+            }else{
+                time = int
+            }
+            answer[j.key]! = fees[1] + (time * fees[3])
+        }
+    }
+    for i in answer.sorted(){ $0.key < $1.key }{
+        b.append(i.value)
+    }
+    return b
 }
