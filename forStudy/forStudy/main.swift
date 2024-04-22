5128e23e4fcd3924a471b349bfdcdf27a1a313e1
inhomun <inho8063@icloud.com>
2024-04-22 12:42:10 +0900

    programmers_점프와_순간이동

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,20 +4,21 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ arr:[Int]) -> Int {
-    // lcm = a*b / gcd(a,b)
-    var b = arr.sorted(by: <)
-    var answer = 1
-    func gcd (_ org: Int,_ tar: Int) {
-        var tmp = 0
-        for j in 1...max(org, tar) where org % j == 0 && tar % j == 0 {
-            tmp = max(tmp, j)
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var a = n
+    var ans = 1
+    while a != 1 {
+        if a % 2 == 0 {
+            a /= 2
         }
-        answer = org * tar / tmp
-        return
+        else {
+            a = (a-1)/2
+            ans += 1
+        }
     }
-    for i in b {
-        gcd(answer, i)
-    }
-    return answer
+    
+    return ans
 }
