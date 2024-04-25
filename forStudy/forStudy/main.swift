e8aba6f9f9d962b538c381641afb066157307b6e
inhomun <inho8063@icloud.com>
2024-04-25 12:11:02 +0900

    programmers_오픈채팅방

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,15 +6,24 @@
 //
 import Foundation
 
-func solution(_ elements:[Int]) -> Int {
-    var answer = [Int]()
-    let cnt = elements.count
-    for k in 0...cnt-1 {
-        var tmp = 0
-        for i in 1...cnt {
-            tmp += elements[(k + i - 1) % cnt]
-            answer.append(tmp)
+func solution(_ record:[String]) -> [String] {
+    var name = [String: String]()
+    var arr = [[String]]()
+    var result = [String]()
+    var a = record.map{ $0.split(separator: " ") }
+    for i in a {
+        arr.append([String(i[0]),String(i[1])])
+        if i[0] != "Leave" {
+            name.updateValue(String(i[2]), forKey: String(i[1]))
         }
     }
-    return Set(answer).count
+    for i in arr {
+        if i[0] == "Enter" {
+            result.append("\(name[i[1]]!)님이 들어왔습니다.")
+        }
+        else if i[0] == "Leave" {
+            result.append("\(name[i[1]]!)님이 나갔습니다.")
+        }
+    }
+    return result
 }
