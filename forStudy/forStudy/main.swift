ad56da14d549b82b3e5f2d5e0ff2a2448368a82b
inhomun <inho8063@icloud.com>
2024-04-27 11:36:16 +0900

    programmers_호텔_대실

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,24 +6,42 @@
 //
 import Foundation
 
-func solution(_ x:Int, _ y:Int, _ n:Int) -> Int {
-    var answer = Array(repeating: Int.max, count: y+1)
-    answer[x] = 0
-    for i in x...y where answer[i] != Int.max {
-        if i+n <= y {
-        answer[i+n] = min(answer[i] + 1, answer[i+n])
+func solution(_ book_time:[[String]]) -> Int {
+    var answer = [[Int]]()
+    var tmp = [[Int]]()
+    for i in book_time {
+        var tmp1 = [Int]()
+        let a = i[0].split(separator: ":")
+        let b = i[1].split(separator: ":")
+        tmp1.append(Int(a[0])! * 60 + Int(a[1])!)
+        tmp1.append(Int(b[0])! * 60 + Int(b[1])! + 10)
+        tmp.append(tmp1)
+    }
+    tmp.sort{ $0[0] < $1[0]}
+    var visited = Array(repeating: false, count: tmp.count)
+    for (idx,i) in tmp.enumerated() {
+        var tmptmp = [Int]()
+        if visited[idx] {
+            continue
         }
-        if i*2 <= y {
-        answer[i*2] = min(answer[i] + 1, answer[i*2])
+        else {
+        var standard = i
+        tmptmp += i
+        
+        for j in idx..<tmp.count  where !visited[j] {
+            if standard[1] <= tmp[j][0] {
+                visited[j] = true
+                standard = tmp[j]
+                tmptmp += standard
+            }
         }
-        if i*3 <= y {
-            answer[i*3] = min(answer[i] + 1, answer[i*3])
+        }
+        if tmptmp.isEmpty {
+            continue
+        }
+        else {
+            answer.append(tmptmp)
         }
     }
-    if answer[y] == Int.max {
-        return -1
-    }
-    else {
-        return answer[y]
-    }
+    return answer.count
 }
