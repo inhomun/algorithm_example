33e644a18f87e82cd0df0f47062114e27a9ca271
inhomun <inho8063@icloud.com>
2024-04-28 12:08:12 +0900

    programmers_가장_큰_수

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,42 +6,11 @@
 //
 import Foundation
 
-func solution(_ book_time:[[String]]) -> Int {
-    var answer = [[Int]]()
-    var tmp = [[Int]]()
-    for i in book_time {
-        var tmp1 = [Int]()
-        let a = i[0].split(separator: ":")
-        let b = i[1].split(separator: ":")
-        tmp1.append(Int(a[0])! * 60 + Int(a[1])!)
-        tmp1.append(Int(b[0])! * 60 + Int(b[1])! + 10)
-        tmp.append(tmp1)
+func solution(_ numbers:[Int]) -> String {
+    var a = numbers.sorted{ Int("\($0)\($1)")! > Int("\($1)\($0)")! }
+    var b = a.map { String($0) }.joined(separator: "")
+    if a[0] == 0 {
+        return "0"
     }
-    tmp.sort{ $0[0] < $1[0]}
-    var visited = Array(repeating: false, count: tmp.count)
-    for (idx,i) in tmp.enumerated() {
-        var tmptmp = [Int]()
-        if visited[idx] {
-            continue
-        }
-        else {
-        var standard = i
-        tmptmp += i
-        
-        for j in idx..<tmp.count  where !visited[j] {
-            if standard[1] <= tmp[j][0] {
-                visited[j] = true
-                standard = tmp[j]
-                tmptmp += standard
-            }
-        }
-        }
-        if tmptmp.isEmpty {
-            continue
-        }
-        else {
-            answer.append(tmptmp)
-        }
-    }
-    return answer.count
+    return b
 }
