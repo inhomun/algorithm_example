55c6ade2c14194951621e63d05b4444fd1b75602
inhomun <inho8063@icloud.com>
2024-04-29 16:46:10 +0900

    programmers_다리_지나가는_트럭

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,33 +6,26 @@
 //
 import Foundation
 
-func solution(_ n:Int, _ wires:[[Int]]) -> Int {
-    var tree = [Int:[Int]]()
-    var answer = [Int]()
-    for wire in wires {
-        tree[wire[0], default: []].append(wire[1])
-        tree[wire[1], default: []].append(wire[0])
-    }
-    func dfs(_ start: Int) -> Int {
-        var queue = [start]
-        var cnt = 0
-        while !queue.isEmpty {
-            var node = queue.removeFirst()
-            visited[node] = true
-            cnt += 1
-            for i in tree[node]! where !visited[i] {
-                queue.append(i)
+func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var wait = truck_weights
+    var bridge = Array(repeating:0, count: bridge_length)
+    var time = 0
+    var sum = 0
+    while !bridge.isEmpty {
+        sum -= bridge.removeFirst()
+        time += 1
+        
+        if !wait.isEmpty {
+            if sum + wait.first! <= weight {
+                let truck = wait.removeFirst()
+                bridge.append(truck)
+                sum += truck
+            }
+            else {
+                bridge.append(0)
             }
         }
-        return cnt
+
     }
-    var visited = Array(repeating: false, count: n + 1)
-    let result = dfs(1)
-    for i in 1...n {
-        visited = Array(repeating: false, count: n + 1)
-        visited[i] = true
-        let count = dfs(1)
-        answer.append(abs(count - (result - count)))
-    }
-    return answer.min()!
+    return time
 }
