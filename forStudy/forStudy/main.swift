4852d20b76c55b6407f5e7aa5bdf4f8e3a624036
inhomun <inho8063@icloud.com>
2024-04-29 21:18:18 +0900

    programmers_의상

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,28 +6,18 @@
 //
 import Foundation
 
-func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
-    var graph = Array(repeating: [(Int,Int)](), count: N + 1)
-    for i in road {
-        graph[i[0]].append((i[1],i[2]))
-        graph[i[1]].append((i[0],i[2]))
+func solution(_ clothes:[[String]]) -> Int {
+    var list = [String:[String]]()
+    var answer = 1
+    var lists = [Int]()
+    for i in clothes {
+        list[i[1], default: []].append(i[0])
     }
-    var distance = Array(repeating: Int.max, count: N + 1)
-    var queue = [(node: 1, dist:0)]
-    distance[1] = 0
-    while !queue.isEmpty {
-        let (node, dist) = queue.removeFirst()
-        
-        if distance[node] < dist {
-            continue
-        }
-        for (nextNode, nextDist) in graph[node] {
-            if dist + nextDist < distance[nextNode] {
-                distance[nextNode] = dist + nextDist
-                queue.append((nextNode, dist + nextDist))
-            }
-        }
+    for i in list {
+        lists.append(i.value.count)
     }
-    
-    return distance.filter{ $0 <= k }.count
+    for (idx, i) in lists.enumerated() {
+        answer *= (i+1)
+    }
+    return answer - 1
 }
