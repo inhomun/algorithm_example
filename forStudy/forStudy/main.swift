b76f5e8597b746145ea4e3f829398fa39eccbd05
inhomun <inho8063@icloud.com>
2024-04-29 14:15:46 +0900

    programmers_전력망을_둘로_나누기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,11 +6,33 @@
 //
 import Foundation
 
-func solution(_ numbers:[Int]) -> String {
-    var a = numbers.sorted{ Int("\($0)\($1)")! > Int("\($1)\($0)")! }
-    var b = a.map { String($0) }.joined(separator: "")
-    if a[0] == 0 {
-        return "0"
+func solution(_ n:Int, _ wires:[[Int]]) -> Int {
+    var tree = [Int:[Int]]()
+    var answer = [Int]()
+    for wire in wires {
+        tree[wire[0], default: []].append(wire[1])
+        tree[wire[1], default: []].append(wire[0])
     }
-    return b
+    func dfs(_ start: Int) -> Int {
+        var queue = [start]
+        var cnt = 0
+        while !queue.isEmpty {
+            var node = queue.removeFirst()
+            visited[node] = true
+            cnt += 1
+            for i in tree[node]! where !visited[i] {
+                queue.append(i)
+            }
+        }
+        return cnt
+    }
+    var visited = Array(repeating: false, count: n + 1)
+    let result = dfs(1)
+    for i in 1...n {
+        visited = Array(repeating: false, count: n + 1)
+        visited[i] = true
+        let count = dfs(1)
+        answer.append(abs(count - (result - count)))
+    }
+    return answer.min()!
 }
