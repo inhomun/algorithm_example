9cd1259b525c8a6917aebdcb9d19b41751d39e75
inhomun <inho8063@icloud.com>
2024-05-14 12:58:41 +0900

    programmers_무인도_여행

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,17 +6,45 @@
 //
 import Foundation
 
-func solution(_ weights:[Int]) -> Int64 {
-    let ratio = [[1,1],[2,4],[4,2],[2,3],[3,2],[3,4],[4,3]]
-    var count = [Int:Int]()
-    var answer = 0
-    for i in weights {
-        for j in ratio {
-            if i * j[0] % j[1] == 0 {
-                answer += count[i * j[0] / j[1], default:0 ]
+func solution(_ maps:[String]) -> [Int] {
+    var map = [[Character]]()
+    for i in maps{
+        map.append(Array(i))
+    }
+    
+    let maxX = maps[0].count
+    let maxY = maps.count
+    var visited = Array(repeating: Array(repeating: false, count: maxX), count: maxY)
+    func dfs(_ x: Int,_ y: Int) -> Int  {
+        if x < 0 || y < 0 || x >= maxX || y >= maxY {
+            return 0
+        }
+        
+        if visited[y][x] == true {
+            return 0
+        }
+        
+        if map[y][x] == "X"{
+            visited[y][x] = true
+            return 0
+        }
+        
+        visited[y][x] = true
+        var tmp = Int(String(map[y][x]))!
+        // 상 하 좌 우로만 움직여야 하기 때문에
+        return tmp + dfs(x, y + 1) + dfs(x, y - 1) + dfs(x + 1, y) + dfs(x - 1, y)
+    }
+   var result = [Int]()
+    
+    for i in 0..<maxY{
+        for j in 0..<maxX{
+            var a = dfs(j, i)
+            if a > 0 {
+                result.append(a)
             }
         }
-        count[i, default:0] += 1
     }
-    return Int64(answer)
+    
+    
+    return result.isEmpty ? [-1] : result.sorted(by: <)
 }
