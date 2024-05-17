1701f9e4d449bf5528af2c6d13c210be4c7630d9
inhomun <inho8063@icloud.com>
2024-05-17 16:59:33 +0900

    programmers_키패드_누르기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,31 +6,47 @@
 //
 import Foundation
 
-func solution(_ X:String, _ Y:String) -> String {
-    var cntA = Array(repeating: 0, count: 10)
-    var cntB = Array(repeating: 0, count: 10)
-    for i in X {
-        cntA[Int(String(i))!] += 1
-    }
-    for j in Y {
-        cntB[Int(String(j))!] += 1
-    }
+func solution(_ numbers:[Int], _ hand:String) -> String {
     var answer = ""
-    for k in stride(from: 9, through: 0, by: -1)  {
-        let minVal = min(cntA[k], cntB[k])
-        if minVal > 0 {
-            for _ in 0..<minVal {
-               answer += String(k)
+    let pos = [1: [0, 0], 2: [0, 1], 3: [0, 2],
+               4: [1, 0], 5: [1, 1], 6: [1, 2],
+               7: [2, 0], 8: [2, 1], 9: [2, 2],
+               0: [3, 1]]
+    var lPos = [3, 0], rPos = [3, 2]
+    func getDistance(_ PosA: [Int],_ PosB: [Int]) -> Int {
+        return abs(PosA[0] - PosB[0]) + abs(PosA[1] - PosB[1])
+    }
+    for i in numbers {
+        if i == 1 || i == 4 || i == 7 {
+            answer += "L"
+            lPos = pos[i]!
+        }
+        else if i == 3 || i == 6 || i == 9 {
+            answer += "R"
+            rPos = pos[i]!
+        }
+        else {
+            let lDist = getDistance(lPos, pos[i]!)
+            let rDist = getDistance(rPos, pos[i]!)
+            if lDist < rDist {
+                answer += "L"
+                lPos = pos[i]!
+            }
+            else if rDist < lDist {
+                answer += "R"
+                rPos = pos[i]!
+            }
+            else {
+                if hand == "right" {
+                    answer += "R"
+                    rPos = pos[i]!
+                }
+                else {
+                    answer += "L"
+                    lPos = pos[i]!
+                }
             }
         }
     }
-    if answer == "" {
-        return "-1"
-    }
-    else if answer.first == "0" {
-        return "0"
-    }
-    else {
-        return answer
-    }
+    return answer
 }
