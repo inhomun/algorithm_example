ad9eefa0e2f21ddb40ec3bb0d82ac7f4f14dab1f
inhomun <inho8063@icloud.com>
2024-05-22 14:39:18 +0900

    programmers_미로_탈출

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,40 +4,70 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ str1:String, _ str2:String) -> Int {
-    var tmp1 = ""
-    var tmp2 = ""
-    var arr1 = [String:Int]()
-    var arr2 = [String:Int]()
-    var arr = [String]()
-    for (x,i) in str1.enumerated() {
-        tmp1 += String(i.uppercased())
-        if tmp1.count == 2 {
-            if tmp1.first!.isLetter && tmp1.last!.isLetter {
-                arr1[tmp1, default:0] += 1
-                arr.append(tmp1)
+import Foundation
+
+func solution(_ maps:[String]) -> Int {
+    var start = [Int]()
+    var lever = [Int]()
+    var map = [[Character]]()
+    var answer1 = [Int]()
+    var answer2 = [Int]()
+    for (x,i) in maps.enumerated() {
+        if i.contains("S") {
+            for (y, j) in Array(i).enumerated() where j == "S" {
+                start.append(x)
+                start.append(y)
             }
-            tmp1.removeFirst()
+        }
+        if i.contains("L") {
+            for (y, j) in Array(i).enumerated() where j == "L" {
+                lever.append(x)
+                lever.append(y)
+            }
+        }
+        map.append(Array(i))
+    }
+    var visited = Array(repeating: Array(repeating: false, count: map[0].count), count: map.count)
+    func bfs(_ startX: Int, _ startY: Int, _ target: Character, _ val: Int) {
+    var queue: [(x: Int, y: Int, val: Int)] = [(startX, startY, val)]
+    
+    let directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    
+    while !queue.isEmpty {
+        let current = queue.removeFirst()
+        let x = current.x
+        let y = current.y
+        let currentVal = current.val
+        
+        if x < 0 || y < 0 || x >= map.count || y >= map[0].count || visited[x][y] || map[x][y] == "X" {
+            continue
+        }
+        
+        if map[x][y] == target {
+            if target == "L" {
+                answer1.append(currentVal)
+            } else {
+                answer2.append(currentVal)
+            }
+            return
+        }
+        
+        visited[x][y] = true
+        
+        for direction in directions {
+            let newX = x + direction.0
+            let newY = y + direction.1
+            queue.append((newX, newY, currentVal + 1))
         }
     }
-    for (y,i) in str2.enumerated() {
-        tmp2 += String(i.uppercased())
-        if tmp2.count == 2 {
-            if tmp2.first!.isLetter && tmp2.last!.isLetter {
-                arr2[tmp2, default:0] += 1
-                arr.append(tmp2)
-            }
-            tmp2.removeFirst()
-        }
+}
+    bfs(start[0],start[1],"L",0)
+    visited = Array(repeating: Array(repeating: false, count: map[0].count), count: map.count)
+    bfs(lever[0],lever[1],"E",0)
+    if answer1.isEmpty || answer2.isEmpty {
+        return -1
     }
-    var inter = 0
-    var union = 0
-    if arr.isEmpty {
-        return 65536
+    else {
+        return answer1.min()! + answer2.min()!
     }
-    for i in Array(Set(arr)) {
-        union += max(arr1[i, default: 0], arr2[i, default: 0])
-        inter += min(arr1[i, default: 0], arr2[i, default: 0])
-    }
-    return Int(Double(inter) / Double(union) * 65536)
 }
