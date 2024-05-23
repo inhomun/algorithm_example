f434fb0f82b85f1386e9c5d79191b4d9e1001d49
inhomun <inho8063@icloud.com>
2024-05-23 19:38:41 +0900

    programmers_압축

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,37 +4,24 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-func solution(_ files:[String]) -> [String] {
-    if files.isEmpty {
-        return []
-    }
-    var list = [(index: Int, name: String, num: String)]()
-    for (idx, file) in files.enumerated() {
-        var name = file
-        var tmp = ""
-        var num = ""
-        while !name.isEmpty {
-            while !name.isEmpty && !name.first!.isNumber {
-                let a = name.removeFirst()
-                tmp += String(a)
-            }
-            while !name.isEmpty && name.first!.isNumber {
-                let a = name.removeFirst()
-                num += String(a)
-            }
-            list.append((idx, tmp, num))
-            break
+func solution(_ msg:String) -> [Int] {
+    var arr = Array(msg)
+    var tmp = String(arr.removeFirst())
+    var num = 0
+    var value = 27
+    var answer = [Int]()
+    var dict : [String:Int] = ["A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26]
+    while !arr.isEmpty {
+        let a = arr.removeFirst()
+        guard let nums = dict[tmp+String(a)] else {
+            dict[tmp+String(a)] = value
+            value += 1
+            answer.append(dict[tmp]!)
+            tmp = String(a)
+            continue
         }
+        tmp += String(a)
     }
-    let sortedList = list.sorted {
-        if $0.name.lowercased() == $1.name.lowercased() {
-            return $0.num < $1.num
-        }
-        return $0.name.lowercased() < $1.name.lowercased()
-    }
-    var answer = [String]()
-    for item in sortedList {
-        answer.append(files[item.index])
-    }
+    answer.append(dict[tmp]!)
     return answer
 }
