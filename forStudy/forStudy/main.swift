0918780e84c9a6540aab87ecc88e6daec5364812
inhomun <inho8063@icloud.com>
2024-05-23 19:38:03 +0900

    programmers_파일명_정렬

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,70 +4,37 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-import Foundation
-
-func solution(_ maps:[String]) -> Int {
-    var start = [Int]()
-    var lever = [Int]()
-    var map = [[Character]]()
-    var answer1 = [Int]()
-    var answer2 = [Int]()
-    for (x,i) in maps.enumerated() {
-        if i.contains("S") {
-            for (y, j) in Array(i).enumerated() where j == "S" {
-                start.append(x)
-                start.append(y)
+func solution(_ files:[String]) -> [String] {
+    if files.isEmpty {
+        return []
+    }
+    var list = [(index: Int, name: String, num: String)]()
+    for (idx, file) in files.enumerated() {
+        var name = file
+        var tmp = ""
+        var num = ""
+        while !name.isEmpty {
+            while !name.isEmpty && !name.first!.isNumber {
+                let a = name.removeFirst()
+                tmp += String(a)
             }
-        }
-        if i.contains("L") {
-            for (y, j) in Array(i).enumerated() where j == "L" {
-                lever.append(x)
-                lever.append(y)
+            while !name.isEmpty && name.first!.isNumber {
+                let a = name.removeFirst()
+                num += String(a)
             }
-        }
-        map.append(Array(i))
-    }
-    var visited = Array(repeating: Array(repeating: false, count: map[0].count), count: map.count)
-    func bfs(_ startX: Int, _ startY: Int, _ target: Character, _ val: Int) {
-    var queue: [(x: Int, y: Int, val: Int)] = [(startX, startY, val)]
-    
-    let directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    
-    while !queue.isEmpty {
-        let current = queue.removeFirst()
-        let x = current.x
-        let y = current.y
-        let currentVal = current.val
-        
-        if x < 0 || y < 0 || x >= map.count || y >= map[0].count || visited[x][y] || map[x][y] == "X" {
-            continue
-        }
-        
-        if map[x][y] == target {
-            if target == "L" {
-                answer1.append(currentVal)
-            } else {
-                answer2.append(currentVal)
-            }
-            return
-        }
-        
-        visited[x][y] = true
-        
-        for direction in directions {
-            let newX = x + direction.0
-            let newY = y + direction.1
-            queue.append((newX, newY, currentVal + 1))
+            list.append((idx, tmp, num))
+            break
         }
     }
-}
-    bfs(start[0],start[1],"L",0)
-    visited = Array(repeating: Array(repeating: false, count: map[0].count), count: map.count)
-    bfs(lever[0],lever[1],"E",0)
-    if answer1.isEmpty || answer2.isEmpty {
-        return -1
+    let sortedList = list.sorted {
+        if $0.name.lowercased() == $1.name.lowercased() {
+            return $0.num < $1.num
+        }
+        return $0.name.lowercased() < $1.name.lowercased()
     }
-    else {
-        return answer1.min()! + answer2.min()!
+    var answer = [String]()
+    for item in sortedList {
+        answer.append(files[item.index])
     }
+    return answer
 }
