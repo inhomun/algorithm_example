4a1d7595bbfdb02c77c02231b52d4ad737d66405
inhomun <inho8063@icloud.com>
2024-05-24 21:11:30 +0900

    programmers_가장_큰_정사각형_찾기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,16 +6,31 @@
 //
 import Foundation
 
-func solution(_ dots:[[Int]]) -> Int {
-    func check(_ dot1: [Int],_ dot2: [Int],_ dot3: [Int],_ dot4: [Int]) -> Int {
-        if Double(abs(dot1[1] - dot2[1])) / Double(abs(dot1[0] - dot2[0])) == Double(abs(dot3[1] - dot4[1])) / Double(abs(dot3[0] - dot4[0])) {
-            return 1
+func solution(_ board:[[Int]]) -> Int
+{
+    if board.count == 1 || board[0].count == 1 {
+        for i in board {
+            if i.contains(1) {
+                return 1
+            }
         }
         return 0
     }
-    var answer = [Int]()
-    answer.append(check(dots[0],dots[1],dots[2],dots[3]))
-    answer.append(check(dots[0],dots[2],dots[1],dots[3]))
-    answer.append(check(dots[0],dots[3],dots[2],dots[1]))
-    return answer.max()!
+    var tmp = board
+    for i in 1..<tmp.count {
+        for j in 1..<tmp[0].count {
+            if tmp[i][j] == 1 {
+                tmp[i][j] += min(tmp[i-1][j],tmp[i][j-1],tmp[i-1][j-1])
+            }
+        }
+    }
+    var answer = 0
+    for i in tmp {
+        answer = max(i.max()!, answer)
+    }
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+
+    return answer*answer
 }
