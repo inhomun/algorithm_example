396b0d232dea24b26f727e2dd458183cabd22b15
inhomun <inho8063@icloud.com>
2024-06-03 16:54:55 +0900

    programmers_섬_연결하기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,46 +6,45 @@
 //
 import Foundation
 
-func solution(_ operations:[String]) -> [Int] {
-    var queue = [Int]()
-    for i in operations {
-        let arr = i.components(separatedBy: " ")
-        if arr[0] == "I" {
-            if queue.isEmpty {
-                queue.append(Int(arr[1])!)
-            }
-            else {
-                if queue.last! <= Int(arr[1])! {
-                    queue.append(Int(arr[1])!)
-                }
-                else {
-                    for i in 0..<queue.count {
-                        if queue[i] >= Int(arr[1])! {
-                            queue.insert(Int(arr[1])!, at: i)
-                            break
-                        }
-                    }
-                }
-            }
+func solution(_ n:Int, _ costs:[[Int]]) -> Int {
+    let sortedcosts = costs.sorted(by: {$0[2] < $1[2] })
+    var parent : [Int] = (0...n-1).map{ $0 }
+    func unionSet(start: Int, end: Int) {
+        var start = start
+        var end = end
+        
+        start = findSet(start)
+        end = findSet(end)
+        if start != end {
+            parent[end] = start
+        }
+    }
+    func findSet(_ s: Int) -> Int {
+        if parent[s] == s {
+            return s
         }
         else {
-            if queue.isEmpty {
-                continue
-            }
-            else {
-                if arr[1] == "-1" {
-                    queue.removeFirst()
+            let parents = findSet(parent[s])
+            parent[s] = parents
+            for i in 0..<parent.count {
+                if parent[i] == s {
+                    parent[i] = parents
                 }
-                else {
-                    queue.popLast()
-                    }
             }
+            return parents
         }
     }
-    if queue.isEmpty {
-        return [0,0]
+    var result = 0
+    for cost in sortedcosts {
+        let start = findSet(cost[0])
+        let end = findSet(cost[1])
+        let value = cost[2]
+        
+        if start != end {
+            result += value
+            unionSet(start: start, end: end)
+        }
     }
-    else {
-        return [queue[queue.count-1], queue[0]]
-    }
+    
+    return result
 }
