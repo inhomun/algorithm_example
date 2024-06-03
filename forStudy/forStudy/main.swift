aebd035935a8e3812ccde1a4901ff308df3f8333
inhomun <inho8063@icloud.com>
2024-06-03 11:21:26 +0900

    programmers_디스크_컨트롤러

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,7 +6,34 @@
 //
 import Foundation
 
-func solution(_ s:String) -> String {
-    let arr = s.split(separator: " ").compactMap { Int($0)! }
-    return "\(arr.min()!) \(arr.max()!)"
+func solution(_ jobs:[[Int]]) -> Int {
+    var now = 0
+    var total = 0
+    var sortedjob = jobs.sorted(by : {
+    if $0[0] == $1[0] {
+        return $0[1] < $1[1]
+    }
+    else {
+        return $0[0] < $1[0]
+    }})
+    var queue = [[Int]]()
+    queue.append(sortedjob.removeFirst())
+    now += queue.first![0]
+    while !queue.isEmpty {
+        let a = queue.removeFirst()
+        total += abs(now - a[0]) + a[1]
+        now += a[1]
+        
+        while !sortedjob.isEmpty && sortedjob.first![0] <=  now {
+            queue.append(sortedjob.removeFirst())
+        }
+        
+        queue.sort(by: {$0[1] < $1[1]})
+        
+        if queue.isEmpty && !sortedjob.isEmpty {
+            queue.append(sortedjob.removeFirst())
+            now = queue.first![0]
+        }
+    }
+    return total / jobs.count
 }
