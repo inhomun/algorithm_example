0c4e819d3061af8b9873c8be6aa4df302fb08581
inhomun <inho8063@icloud.com>
2024-06-03 15:08:23 +0900

    programmers_이중우선순위큐

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,34 +6,46 @@
 //
 import Foundation
 
-func solution(_ jobs:[[Int]]) -> Int {
-    var now = 0
-    var total = 0
-    var sortedjob = jobs.sorted(by : {
-    if $0[0] == $1[0] {
-        return $0[1] < $1[1]
+func solution(_ operations:[String]) -> [Int] {
+    var queue = [Int]()
+    for i in operations {
+        let arr = i.components(separatedBy: " ")
+        if arr[0] == "I" {
+            if queue.isEmpty {
+                queue.append(Int(arr[1])!)
+            }
+            else {
+                if queue.last! <= Int(arr[1])! {
+                    queue.append(Int(arr[1])!)
+                }
+                else {
+                    for i in 0..<queue.count {
+                        if queue[i] >= Int(arr[1])! {
+                            queue.insert(Int(arr[1])!, at: i)
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            if queue.isEmpty {
+                continue
+            }
+            else {
+                if arr[1] == "-1" {
+                    queue.removeFirst()
+                }
+                else {
+                    queue.popLast()
+                    }
+            }
+        }
+    }
+    if queue.isEmpty {
+        return [0,0]
     }
     else {
-        return $0[0] < $1[0]
-    }})
-    var queue = [[Int]]()
-    queue.append(sortedjob.removeFirst())
-    now += queue.first![0]
-    while !queue.isEmpty {
-        let a = queue.removeFirst()
-        total += abs(now - a[0]) + a[1]
-        now += a[1]
-        
-        while !sortedjob.isEmpty && sortedjob.first![0] <=  now {
-            queue.append(sortedjob.removeFirst())
-        }
-        
-        queue.sort(by: {$0[1] < $1[1]})
-        
-        if queue.isEmpty && !sortedjob.isEmpty {
-            queue.append(sortedjob.removeFirst())
-            now = queue.first![0]
-        }
+        return [queue[queue.count-1], queue[0]]
     }
-    return total / jobs.count
 }
