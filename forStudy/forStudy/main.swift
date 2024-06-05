f5140c5b450d74c5f565e8aa897e22ca28df2115
inhomun <inho8063@icloud.com>
2024-06-05 12:44:54 +0900

    programmers_베스트앨범

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,23 +6,25 @@
 //
 import Foundation
 
-func solution(_ a:[Int], _ b:[Int]) -> Int {
-    let sortA = a.sorted(by: <)
-    let sortB = b.sorted(by: <)
-    let max = sortB.count
-    var answer = 0
-    var start = 0
-    for i in sortA {
-        for j in start..<max {
-            if i < sortB[j] {
-                    start = j+1
-                    answer += 1
-                    break
-            }
-        }
-        if start == max {
+func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
+    var total = [String:Int]()
+    var info = [String:[(Int, Int)]]()
+    for i in 0..<genres.count {
+        total[genres[i], default: 0] += plays[i]
+        info[genres[i], default: []].append((i, plays[i]))
+    }
+    var answer = [Int]()
+    let sortedgenres = total.keys.sorted(by: { total[$0]! > total[$1]! })
+    for i in sortedgenres {
+        let a = info[i]!.sorted(by : { $0.1 > $1.1 } )
+        var cnt = 0
+        for i in a {
+            if cnt == 2 {
                 break
             }
+            answer.append(i.0)
+            cnt += 1
+        }
     }
     return answer
 }
