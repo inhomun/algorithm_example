f4347f82f8c97a46eb3a9b5e3dabcb7aa8a43d18
inhomun <inho8063@icloud.com>
2024-06-12 16:58:53 +0900

    programmers_모음사전

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,62 +6,25 @@
 //
 import Foundation
 
-func solution(_ rows:Int, _ columns:Int, _ queries:[[Int]]) -> [Int] {
-    var answer = [Int]()
-    var arr1 = [Int]()
-    var map1 = [[Int]]()
-    var map2 = [[Int]]()
-    var val = 0
-    for i in 0..<rows {
-        var tmp = [Int]()
-        for j in 0..<columns {
-            val += 1
-            tmp.append(val)
+func solution(_ word:String) -> Int {
+    var answer = word.replacingOccurrences(of: "A" , with: "1")
+    answer = answer.replacingOccurrences(of: "E" , with: "2")
+    answer = answer.replacingOccurrences(of: "I" , with: "3")
+    answer = answer.replacingOccurrences(of: "O" , with: "4")
+    answer = answer.replacingOccurrences(of: "U" , with: "5")
+    var target = Array(answer).compactMap { Int(String($0))! }
+    var tmp = [Int]()
+    var cnt = 0
+    while tmp != target {
+        cnt += 1
+        if tmp.count != 5 {
+            tmp.append(1)
+            continue
         }
-        map1.append(tmp)
-        map2.append(tmp)
+        while tmp.last == 5 {
+            tmp.popLast()!
+        }
+        tmp[tmp.count-1] += 1
     }
-    func rotate(_ arr: [Int],_ x: Int,_ y: Int,_ val: Int) {
-        if map2[x][y] == val {
-            answer.append(arr1.min()!)
-            arr1 = []
-            map1 = map2
-            return
-        }
-        arr1.append(val)
-        map2[x][y] = val
-        
-        if x == arr[0]-1 {
-            if y == arr[3]-1 {
-                rotate(arr, x+1, y, map1[x][y])
-            }
-            else {
-                rotate(arr, x, y+1, map1[x][y])
-            }
-        }
-        else if y == arr[3]-1 {
-            if x == arr[2]-1 {
-                rotate(arr, x, y-1, map1[x][y])
-            }
-            else {
-                rotate(arr, x+1, y, map1[x][y])
-            }
-        }
-        else if x == arr[2]-1 {
-            if y == arr[1]-1 {
-                rotate(arr, x-1, y, map1[x][y])
-            }
-            else {
-                rotate(arr, x, y-1, map1[x][y])
-            }
-        }
-        else if y == arr[1]-1 {
-            rotate(arr, x-1, y, map1[x][y])
-        }
-        return
-    }
-    for query in queries {
-        rotate(query, query[0]-1, query[1]-1, map1[query[0]][query[1]-1])
-    }
-    return answer
+    return cnt
 }
