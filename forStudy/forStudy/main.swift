260a3571eea228863fecd945b5e40de452b10051
inhomun <inho8063@icloud.com>
2024-06-12 19:58:47 +0900

    programmers_입국심사

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,25 +6,25 @@
 //
 import Foundation
 
-func solution(_ word:String) -> Int {
-    var answer = word.replacingOccurrences(of: "A" , with: "1")
-    answer = answer.replacingOccurrences(of: "E" , with: "2")
-    answer = answer.replacingOccurrences(of: "I" , with: "3")
-    answer = answer.replacingOccurrences(of: "O" , with: "4")
-    answer = answer.replacingOccurrences(of: "U" , with: "5")
-    var target = Array(answer).compactMap { Int(String($0))! }
-    var tmp = [Int]()
-    var cnt = 0
-    while tmp != target {
-        cnt += 1
-        if tmp.count != 5 {
-            tmp.append(1)
-            continue
+func solution(_ n:Int, _ times:[Int]) -> Int64 {
+    var time = times.sorted(by: <)
+    var minTime = 1
+    var maxTime = time.last! * n
+    var answer = 0
+    while minTime <= maxTime {
+        let midTime = (minTime + maxTime) / 2
+        var cnt = 0
+        time.forEach {
+            cnt += midTime / $0
         }
-        while tmp.last == 5 {
-            tmp.popLast()!
+        
+        if cnt < n {
+            minTime = midTime + 1
         }
-        tmp[tmp.count-1] += 1
+        else {
+            maxTime = midTime - 1
+            answer = midTime
+        }
     }
-    return cnt
+    return Int64(answer)
 }
