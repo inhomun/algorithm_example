8703bc9eea036f7f77620af69050f4161cbb13ee
inhomun <inho8063@icloud.com>
2024-06-12 16:35:25 +0900

    programmers_행렬_테두리_회전

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -6,21 +6,62 @@
 //
 import Foundation
 
-func solution(_ gems:[String]) -> [Int] {
-    let target = Set(gems).count
-    var answer = [1, Int.max]
-    var dict = [String:Int]()
-    var start = 0
-    for i in 0..<gems.count {
-        dict[gems[i], default: 0] += 1
-        while start <= i && dict[gems[start]]! > 1 {
-            dict[gems[start]]! -= 1
-            start += 1
+func solution(_ rows:Int, _ columns:Int, _ queries:[[Int]]) -> [Int] {
+    var answer = [Int]()
+    var arr1 = [Int]()
+    var map1 = [[Int]]()
+    var map2 = [[Int]]()
+    var val = 0
+    for i in 0..<rows {
+        var tmp = [Int]()
+        for j in 0..<columns {
+            val += 1
+            tmp.append(val)
         }
+        map1.append(tmp)
+        map2.append(tmp)
+    }
+    func rotate(_ arr: [Int],_ x: Int,_ y: Int,_ val: Int) {
+        if map2[x][y] == val {
+            answer.append(arr1.min()!)
+            arr1 = []
+            map1 = map2
+            return
+        }
+        arr1.append(val)
+        map2[x][y] = val
         
-        if dict.count == target && answer[1] - answer[0] > i - start {
-            answer = [start+1, i+1]
+        if x == arr[0]-1 {
+            if y == arr[3]-1 {
+                rotate(arr, x+1, y, map1[x][y])
+            }
+            else {
+                rotate(arr, x, y+1, map1[x][y])
+            }
         }
+        else if y == arr[3]-1 {
+            if x == arr[2]-1 {
+                rotate(arr, x, y-1, map1[x][y])
+            }
+            else {
+                rotate(arr, x+1, y, map1[x][y])
+            }
+        }
+        else if x == arr[2]-1 {
+            if y == arr[1]-1 {
+                rotate(arr, x-1, y, map1[x][y])
+            }
+            else {
+                rotate(arr, x, y-1, map1[x][y])
+            }
+        }
+        else if y == arr[1]-1 {
+            rotate(arr, x-1, y, map1[x][y])
+        }
+        return
+    }
+    for query in queries {
+        rotate(query, query[0]-1, query[1]-1, map1[query[0]][query[1]-1])
     }
     return answer
 }
