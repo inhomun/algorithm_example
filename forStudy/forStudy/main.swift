91497651f83b1a0e682e72112f663fde0cae09b9
inhomun <inho8063@icloud.com>
2024-06-13 11:30:06 +0900

    goorm_goormsquare

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -4,27 +4,21 @@
 //
 //  Created by 문인호 on 2023/01/27.
 //
-import Foundation
-
-func solution(_ n:Int, _ times:[Int]) -> Int64 {
-    var time = times.sorted(by: <)
-    var minTime = 1
-    var maxTime = time.last! * n
-    var answer = 0
-    while minTime <= maxTime {
-        let midTime = (minTime + maxTime) / 2
-        var cnt = 0
-        time.forEach {
-            cnt += midTime / $0
-        }
-        
-        if cnt < n {
-            minTime = midTime + 1
-        }
-        else {
-            maxTime = midTime - 1
-            answer = midTime
-        }
-    }
-    return Int64(answer)
+let input = readLine()!
+var festival = [[Int]]()
+for _ in 0..<Int(input)! {
+    let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+    festival.append(arr)
 }
+
+festival.sort(by: { $0[1] < $1[1] } )
+var answer = 0
+var time = 0
+for i in festival {
+        if i[0] > time {
+            answer += 1
+            time = i[1]
+        }
+    
+}
+print("\(answer)")
