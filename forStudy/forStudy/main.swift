7239badbe6ed86eeb85bcf6cf7d9fbde9cdec5b3
inhomun <inho8063@icloud.com>
2024-06-13 11:59:02 +0900

    goorm_개발자지망생구름이의취업뽀개기

--- forStudy/forStudy/main.swift
+++ forStudy/forStudy/main.swift
@@ -5,20 +5,34 @@
 //  Created by 문인호 on 2023/01/27.
 //
 let input = readLine()!
-var festival = [[Int]]()
+let prob = readLine()!.split(separator: " ").map{ Int($0)! }
+var times = [[Int]]()
 for _ in 0..<Int(input)! {
-    let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
-    festival.append(arr)
+    let value = readLine()!.split(separator: " ").map{ Int($0)! }
+    times.append(value)
 }
-
-festival.sort(by: { $0[1] < $1[1] } )
+times.sort(by : { if $0[0] == $1[0] { return $0[1] < $1[1]}
+                                else {
+                                    return $0[0] < $1[0]
+                                }})
 var answer = 0
-var time = 0
-for i in festival {
-        if i[0] > time {
-            answer += 1
-            time = i[1]
+var arr = [times[0][0],times[0][1]]
+answer += arr[1]
+times.removeFirst()
+var cnt = 1
+for a in times {
+    if a[0] == arr[0] {
+        if cnt == prob[a[0]-1] {
+            continue
         }
-    
+        answer += (a[1] - arr[1])
+        cnt += 1
+    }
+    else {
+        cnt = 1
+        answer += 60
+    }
+    answer += a[1]
+    arr = [a[0],a[1]]
 }
 print("\(answer)")
